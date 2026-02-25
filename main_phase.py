58040748c04fe24f19d72fd88b2ec935dfa5c556
Mars Borleis <marsborleis@gmail.com>
2026-02-24 23:13:16 -0500

    core work start

--- main_phase.py
+++ main_phase.py
@@ -1,4 +1,27 @@
 from start import *
+import sys
 
 def core_sequence(deck):
-    shuffle(deck)
+    mutable = deck.copy()
+    shuffle(mutable)
+    step = input("Enter one:\nmark cards\nshuffle\ndraw card\ndraw hand\nexit\n")
+    match step:
+        case "mark cards":
+            pass
+        case "shuffle":
+            shuffle(mutable)
+            print("Deck shuffled")
+        case "draw card":
+            card = mutable[0]
+            del mutable[0]
+            print(card.name)
+        case "draw hand":
+            hand = mutable[0:7]
+            del mutable[0:7]
+            for card in hand:
+                print(card.name)
+        case "exit":
+            sys.exit()
+
+deck = start_sequence()
+core_sequence(deck)
